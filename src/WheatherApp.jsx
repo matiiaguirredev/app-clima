8ccb6aa5b9162ce0712e0413e99d7cb3adf3dd7a
matiidev <matiasagui93@gmail.com>
2023-12-16 11:11:57 +0100

    app de clima terminada

---
+++ src/WheatherApp.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+
+
+export const WheatherApp = () => {
+
+    const urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+    const API_KEY = '814da86aae748ae5053c4a36227fa05c'
+    const difKelvyn = 273.15
+
+    const [ciudad, setCiudad] = useState('')
+    const [dataClima, setDataClima] = useState(null)
+
+    const handleCambioCiudad = (e) => {
+        setCiudad(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(ciudad.length > 0){
+            fetchClima()
+        }
+    }
+
+    const fetchClima = async () => {
+        try{
+            const response = await fetch(`${urlBase}?q=${ciudad}&appid=${API_KEY}`)
+            const data = await response.json()
+            setDataClima(data)
+        }catch(error){
+            console.error("Ocurrio el siguiente problema: ", error)
+        }
+    }
+
+
+    return (
+
+    <div className="container">
+        <h1>Aplicacion de Clima</h1>
+
+        <form onSubmit={handleSubmit}>
+            <input value={ciudad} onChange={handleCambioCiudad} type="text" />
+            <button type="submit">Buscar</button>
+        </form>
+        {
+            dataClima && (
+                <div>
+                    <h2>{dataClima.name}</h2>
+                    <p>Temperatura: {parseInt(dataClima?.main?.temp - difKelvyn)}°C</p>
+                    <p>Condicion meteorologica: {dataClima.weather[0].description}</p>
+                    <img src={`https://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`}/>
+                </div>
+            )
+        }
+
+    </div>
+
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { WheatherApp } from './WheatherApp'
+import './styles/styles.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WheatherApp></WheatherApp>
+  </React.StrictMode>,
+)
